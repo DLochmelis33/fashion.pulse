8fe28affdaf61f38c4d7bfa9f0782d308ce416f6
Denis Lochmelis <dlochmelis@gmail.com>
2023-12-03 20:45:02 +0100

    Add healthcheck endpoint

--- server/server.py
+++ server/server.py
@@ -5,6 +5,11 @@ from fastapi import FastAPI, UploadFile
 app = FastAPI()
 
 
-@app.post("/")
+@app.get("/")
+async def healthcheck():
+    return "OK"
+
+
+@app.post("/analyze")
 async def upload_image(image: UploadFile):
     return {"scores": {"hiphop": 0.9, "renaissance": 0.0, "gleb": 6.5}}
